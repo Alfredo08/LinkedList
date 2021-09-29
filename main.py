c2716302057807b4e25fa2a2c7a3c9bee2fdb071
Alfredo08 <alfredosv08@gmail.com>
2021-09-29 10:41:14 -0500

    Instructions for min and max values added

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ listOfNumbers.insertLast( 30 )
 listOfNumbers.insertAtPosition( 5, 50 )
 listOfNumbers.printList()
 
+
+# Find the maximun value inside a list of numbers and move it to the last node
+
+# Find the minimum value inside a list of numbers and move it to the head of the list
+
